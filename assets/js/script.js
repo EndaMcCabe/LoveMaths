a58807796078273900257a7985abbb75377b3b50
EndaMcCabe <endasteven.mccabe@gmail.com>
2022-11-18 21:22:03 +0000

    Implement the increment score and increment wrong answer functions and update the tallies through the check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,9 +44,10 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if(isCorrect) {
-        alert("Hey! You got it right! :D");
+        incrementScore();
     } else{
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -69,12 +70,21 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById("score").innerText = ++oldScore;
+    //put the ++ before the variable so js puts in the updated score not the original
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
