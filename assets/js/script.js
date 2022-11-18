8afea8a06517eae6ab08bb41d28e953dc6e03efa
EndaMcCabe <endasteven.mccabe@gmail.com>
2022-11-18 22:01:27 +0000

    Add favicon, add tidyups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -22,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType) {
 
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //Creates two random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -30,6 +39,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if(gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
+    } else if(gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -68,7 +79,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if(operator === "x"){
         return [operand1 * operand2, "multiply"];
-    }   else{
+    } else if(operator === "-"){
+        return [operand1 - operand2, "subtract"];
+    } else{
         alert(`Unimplemented operator $[operator]`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -97,8 +110,12 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = '+';
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    //which is bigger: operand1 or operand2? If 1 is bigger return that | If operand2 is bigger return that
+    // condition ? true part : false part;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-';
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
